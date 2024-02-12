f026497df9d2c5da7c94f26c46a4c8e7fddc57af
luismdsleite <luismdsleite@gmail.com>
2024-02-12 01:49:13 +0000

    First Question

--- app.mjs
+++ app.mjs
@@ -6,6 +6,15 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.redirect('q1');
+});
+
+app.get('/q1', (req, res) => {
+    res.sendStatus(200);
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
